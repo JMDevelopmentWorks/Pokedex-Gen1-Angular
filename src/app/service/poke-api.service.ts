676148d652fcc99669cd0c2b47892489d38e4c53
JMDevelopmentWork <joaomatos.developmentwork@gmail.com>
2023-04-21 09:16:15 +0100

    small change to the limit

--- src/app/service/poke-api.service.ts
+++ src/app/service/poke-api.service.ts
@@ -8,7 +8,7 @@ import { Observable, map, tap } from 'rxjs';
   providedIn: 'root',
 })
 export class PokeApiService {
-  private url: string = 'https://pokeapi.co/api/v2/pokemon/?offset=0&limit=151';
+  private url: string = 'https://pokeapi.co/api/v2/pokemon/?offset=0&limit=100';
 
   constructor(private http: HttpClient) {}
 
